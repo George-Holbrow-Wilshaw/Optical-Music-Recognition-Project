c11e34f1f34f97fc0365c531b489a9ca038eb34c
George Holbrow-Wilshaw <georgewilshaw1996@gmail.com>
2020-12-22 17:06:00 +0000

    image processing classes and obj creation

--- bin/image_processing.py
+++ bin/image_processing.py
@@ -4,15 +4,29 @@ import re
 
 class primusSequence:
 
-    def __init__(self, folder):
-        self.image = imageio.imread("./data/package_aa/" + folder + "/" + folder + ".png")
-        self.semantic = open("./data/package_aa/" + folder + "/" + folder + ".semantic").read()
+    def __init__(self, folder, package):
+        self.image = imageio.imread("./data/" + package + "/" + folder + "/" + folder + ".png")
+        self.semantic = open("./data/" + package + "/" + folder + "/" + folder + ".semantic").read()
         self.labels = re.findall(r'note\-(.+?)\_', self.semantic)
         
-def join_images_labels(labels, images):
-    pairs = dict()
-    for l, i in zip(labels, images):
-        pairs[l] = i   
-    return(pairs)
+    def flattened_image(self):
+        im = self.image
+        return(im.flatten())
+                                    
+        
+def init_objs(dataset):
+    
+    if dataset == 'train':
+        ROOT = './data/package_aa/'
+        package = 'package_aa'
+    elif dataset == 'validate':
+        ROOT = './data/package_ab/'
+        package = 'package_ab'
 
-
+    folders = [directory for directory in os.listdir(ROOT) if os.path.isdir(ROOT+directory)]
+    objs = []
+    
+    for f in folders:
+        objs.append(primusSequence(f, package))
+        
+    return(objs)
